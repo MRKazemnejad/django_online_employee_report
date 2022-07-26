d1c3d15b0735a6c4e5a2a7e76505cd53d34cd438
Kazemnezhad Mohamadreza <kazemnezhad_m@mapnamrm.local>
2022-07-26 11:42:11 +0430

    create project

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class UserLoginView(View):
+    pass
+
+
+class UserLogoutView(View):
+    pass
+
+
+class UserRegisterView(View):
+    pass

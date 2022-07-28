561ebaac4e4d4bd78013c0af15f69439d4a39e0a
Kazemnezhad Mohamadreza <kazemnezhad_m@mapnamrm.local>
2022-07-28 14:25:06 +0430

    add images to hom page

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Product,Category
 
 
 class UserHomeView(View):
     def get(self,request):
-         return render(request,'shop/index.html')
+         product=Product.objects.all()
+         return render(request,'shop/index.html',{'products':product})
 
 
 class UserWorkshopView(View):
